39c23d55b3096ace0e74131a67675f2c497ed9a8
Yuri Alves de Almeida <yurialves2@gmail.com>
2018-04-25 05:36:04 -0300

    Inserindo dados

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,7 +1,17 @@
 module.exports = function(app) {
+  let service = require('../services/contacts')(app);
+
   return {
     index: (req, res) => res.render('index'),
     new: (req, res) => res.render('new'),
-    newPost: (req, res) => res.send('//TODO: Formulário recebido')
+    newPost: (req, res) => {
+      let newContact = service.push();
+      newContact.set({
+        name: req.body.name,
+        email: req.body.email
+      });
+      // res.send('Formulário recebido: ' + JSON.stringify(req.body))
+      res.redirect('/');
+    }
   }
 }
